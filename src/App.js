471da53c7a9f9ec99819d027c16d6d478c5a54d3
alejandro <alejandrober96@gmail.com>
2023-03-23 11:50:27 -0500

    linters

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Calculator from './components/calculator';
+import ApiCall from './components/apiCall';
 
 function App() {
   return (
-    <Calculator />
+    <div className="calculator-holder">
+      <Calculator />
+      <ApiCall />
+    </div>
   );
 }
 
